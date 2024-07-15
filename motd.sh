a1f45549850c519f9b495aed18910b56e631a97a
Fabian Juel Escalera Caballero <fabianjuelescalera@gmail.com>
2024-07-14 20:55:23 -0400

    motd agregado como aliaas

---
+++ motd.sh
@@ -0,0 +1,17 @@
+alias motd="run-parts /etc/update-motd.d/"
+
+#!/bin/bash
+
+# Nombre del archivo donde se añadirá el alias
+path0=$HOME
+path1="/.bashrc"
+
+archivo=$path0$path1
+
+# Alias a añadir
+alias_motd='alias motd="run-parts /etc/update-motd.d/"'
+
+# Añadir el alias al archivo
+echo "$alias_motd" >> "$archivo"
+
+echo "Alias añadido correctamente al archivo."
